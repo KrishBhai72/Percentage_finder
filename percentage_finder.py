4103f2d23a25463b636f5cf63f772768dbc5b783
KrishBhai72 <igadontno@gmail.com>
2025-03-27 09:11:42 +0530

    percentage_finder.py
    This is a simple and efficient Python program designed to calculate the percentage of a given number. The program takes an input value and a total value, then calculates and returns the percentage. It provides a straightforward solution for anyone needing to quickly find the percentage of a number without any unnecessary complexity.

---
+++ percentage_finder.py
@@ -0,0 +1,22 @@
+# This is my real project
+import time
+
+
+print('Welcome to result finder')
+time.sleep(2)
+
+g = (int(input("Enter How Many Exams you gave:- ")))
+
+a = int(input("Enter Your English subject Marks:- ")) 
+b = int(input("Enter Your Maths subject Marks:- "))
+c = int(input("Enter Your Science subject Marks:- "))
+d = int(input("Enter Your S.S subject Marks:- ")) 
+e= int(input("Enter Your Hindi subject Marks:- "))
+f = int(input("Enter Your Computer subject Marks:- "))
+
+
+result = (a + b + c + d + e + f)
+print(result/g)
+
+
+# First programm buddy made without any use of ai
